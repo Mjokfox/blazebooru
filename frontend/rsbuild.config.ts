62030e7290f5a82f16afa326c2e0d67251ffc425
Forb.Jok <forbjok@proton.me>
2025-04-17 22:50:32 +0200

    chore: Use rsbuild

---
+++ frontend/rsbuild.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "@rsbuild/core";
+import { pluginSass } from "@rsbuild/plugin-sass";
+import { pluginVue } from "@rsbuild/plugin-vue";
+
+export default defineConfig({
+  plugins: [pluginSass(), pluginVue()],
+  source: {
+    alias: {
+      "@": "./src",
+    },
+  },
+  html: {
+    template: "./index.html",
+    favicon: "./favicon.ico",
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      "/api/": {
+        target: "http://localhost:3000",
+        xfwd: true,
+      },
+      "/f/": {
+        target: "http://localhost:3000",
+      },
+    },
+  },
+});
